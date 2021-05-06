a57dff2ba151383f1c1ebb141570c5d8a2d975f2
zhouhuafei <1123486116@qq.com>
2021-05-06 09:23:52 +0800

    feat: 🎸 token不存在且req.url不在白名单中则return。

--- service-api/app/service/send.js
+++ service-api/app/service/send.js
@@ -10,12 +10,21 @@ class MyService extends Service {
     try {
       let [project, env, item] = type.split('.')
       let { permissionApi, webHookUrl, name } = sysConfig[project][env][item]
-      const permissionRes = await axios({
-        url: permissionApi,
-        headers: { token: (req.header || req.headers).token || '' }
-      })
-      if (permissionRes.data.status === 401) {
-        return {}
+      const token = (req.header || req.headers).token || ''
+      if (token) {
+        // token存在但是token过期了则return。
+        const permissionRes = await axios({
+          url: permissionApi,
+          headers: { token }
+        })
+        if (permissionRes.data.status === 401) {
+          return {}
+        }
+      } else {
+        // token不存在且req.url不在白名单中则return。
+        // 白名单中应包含pc管理系统中的当前用户所属租户列表接口，登录接口，验证码接口。
+        // 白名单中应包含mp客户端中的登录接口。
+        // 此处仅做文案记录并不打算进行开发。
       }
       await apiSend.send({
         url: webHookUrl,
